3c7131584ee3e949c7c7d9e7d3945eaf44c9f707
HamazakiHisanori <hiii1023@icloud.com>
2021-09-03 17:15:12 +0900

    Login page create

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,22 @@
 import { memo, VFC } from 'react';
+import { Box, Divider, Flex, Heading, Stack } from '@chakra-ui/layout';
+import { Input } from '@chakra-ui/input';
+import { Button } from '@chakra-ui/button';
+
 
 export const Login: VFC = memo(() => {
-  return <p>Log In</p>
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" padding={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザー管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={3} py={4} px={10}>
+          <Input placeholder="ユーザーID" />
+          <Button bg="teal.400" color="white" _hover={{ opacity: 0.6 }}>ログイン</Button>
+        </Stack>
+      </Box>
+    </Flex>
+  )
 });
